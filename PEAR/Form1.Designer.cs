8cb56c494a989a5882b5db106bd8e3a1de32b7e6
itsung26 <iat2016@icloud.com>
2026-06-09 13:00:00 -0400

    Add project files.

---
+++ PEAR/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace PEAR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lagStatusIndicatorTextFront = new System.Windows.Forms.Label();
+            this.offLabel = new System.Windows.Forms.Label();
+            this.keybindSelector = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::PEAR.Properties.Resources.pear32;
+            this.pictureBox1.Location = new System.Drawing.Point(114, 301);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(59, 60);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lagStatusIndicatorTextFront
+            // 
+            this.lagStatusIndicatorTextFront.AutoSize = true;
+            this.lagStatusIndicatorTextFront.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lagStatusIndicatorTextFront.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lagStatusIndicatorTextFront.Location = new System.Drawing.Point(44, 109);
+            this.lagStatusIndicatorTextFront.Name = "lagStatusIndicatorTextFront";
+            this.lagStatusIndicatorTextFront.Size = new System.Drawing.Size(144, 32);
+            this.lagStatusIndicatorTextFront.TabIndex = 6;
+            this.lagStatusIndicatorTextFront.Text = "lag status:";
+            // 
+            // offLabel
+            // 
+            this.offLabel.AutoSize = true;
+            this.offLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.offLabel.ForeColor = System.Drawing.Color.Red;
+            this.offLabel.Location = new System.Drawing.Point(183, 111);
+            this.offLabel.Name = "offLabel";
+            this.offLabel.Size = new System.Drawing.Size(70, 32);
+            this.offLabel.TabIndex = 7;
+            this.offLabel.Text = "OFF";
+            // 
+            // keybindSelector
+            // 
+            this.keybindSelector.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.keybindSelector.Location = new System.Drawing.Point(86, 144);
+            this.keybindSelector.Name = "keybindSelector";
+            this.keybindSelector.ReadOnly = true;
+            this.keybindSelector.Size = new System.Drawing.Size(133, 26);
+            this.keybindSelector.TabIndex = 8;
+            this.keybindSelector.Text = "enter a keybind...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(296, 373);
+            this.Controls.Add(this.keybindSelector);
+            this.Controls.Add(this.offLabel);
+            this.Controls.Add(this.lagStatusIndicatorTextFront);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PEAR lagswitch";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lagStatusIndicatorTextFront;
+        private System.Windows.Forms.Label offLabel;
+        private System.Windows.Forms.TextBox keybindSelector;
+    }
+}
+
---
+++ PEAR/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PEAR
+{
+    public partial class Form1 : Form
+    {
+        private String boundButtonName;
+        private String boundButtonCode;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
